e7efe46e9d855d1674914213d07450dfae182389
Lily Zhou <lzhou0714@g.ucla.edu>
2022-11-27 19:43:10 -0800

    curve placememt

--- maps/map1.js
+++ maps/map1.js
@@ -57,9 +57,15 @@ export class GameMap extends BaseMap {
 		// 	this.materials.road
 		// );
 
-		this.draw_road(context, program_state, 10, 50);
-		this.draw_road(context, program_state, 10, 50);
-		this.draw_curve(context, program_state);
+		this.draw_road(context, program_state, 10, 10);
+		this.draw_road(context, program_state, 10, 10);
+		//r = turn right, l = turn left
+		this.draw_curve(context, program_state, 'l');
+		this.draw_road(context, program_state, 10, 10);
+		this.draw_curve(context, program_state, 'r');
+		this.draw_road(context, program_state, 10, 10);
+		// this.draw_road(context, program_state, 10, 10);
+
 		// let track1l_collider = new Box_Collider(-11, -101, 2, 202);
 		// let track1r_collider = new Box_Collider(9, -101, 2, 202);
 		// this.colliders[8] = track1l_collider;
